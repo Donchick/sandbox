1152ff786552b4077a1c2ffdda5cff39a204eebe
donattravin@gmail.com <dtravin@google.com>
2020-10-29 11:28:22 -0700

    added simple pub/sub infrastricture

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require('body-parser');
+// Imports the Google Cloud client library
+const {PubSub} = require('@google-cloud/pubsub');
 
 const PORT = process.env.PORT || 5001;
 
 const app = express();
+const pubsub = new PubSub({projectId: "white-inscriber-293222", keyFilename: "white-inscriber-293222-43c182cef4f1.json"});
+const defaultTopic = pubsub.topic("projects/white-inscriber-293222/topics/signup-notificator");
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use("/", express.static(path.join(__dirname, "assets")));
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get("/signup", (req, res) => {
+  res.sendFile(path.join(__dirname+'/signup.html'));
+});
+
+app.post("/signup", async (req, res) => {
+  await defaultTopic.publish(Buffer.from(JSON.stringify(req.body)));
+  res.send("OK");
 });
 
 app.get("/test", (req, res) => {
